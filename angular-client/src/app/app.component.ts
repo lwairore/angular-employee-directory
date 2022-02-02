c5b066e88c9e19263ee8fed45f3a43b3015abbb7
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 21:27:25 +0300

    - Initialize 'addPersonSubscription'.

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  addPersonSubscription: Subscription;
+
   constructor() { }
   ngOnInit() { }
 }
