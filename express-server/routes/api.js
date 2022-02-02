82cc6c19628eafdc836e89100de88bec66d32580
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 21:13:09 +0300

    - Connect to 'mongodb'.

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -5,6 +5,9 @@ const router = express.Router();
 // MongoDB URL from the docker-compose file
 const dbHost = 'mongodb://database/mean-docker';
 
+// Connect to mongodb
+mongoose.connect(dbHost);
+
 // GET api listing.
 router.get('/', (req, res) => {
     res.send('api works');
