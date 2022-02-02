f660d91a36464c63ba6cb78a9027412982eebaae
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 21:15:14 +0300

    - Create api to create a user.

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -40,5 +40,20 @@ router.get('/users/:id', (req, res) => {
     });
 });
 
+// Create a user.
+router.post('/users', (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        age: req.body.age
+    });
+
+    user.save(error => {
+        if (error) res.status(500).send(error);
+
+        res.status(201).json({
+            message: 'User created successfully'
+        });
+    });
+});
 
 module.exports = router;
