16379023c50a749aea0e4aec64ea4d747644ef15
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 20:55:23 +0300

    - Set the parsers for 'POST' data.

--- express-server/server.js
+++ express-server/server.js
@@ -8,3 +8,7 @@ const bodyParser = require('body-parser');
 const api = require('./routes/api');
 
 const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
