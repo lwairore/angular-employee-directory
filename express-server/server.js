18bb9f50c1c10753d73537cc0760177868a1d144
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 20:56:40 +0300

    - Listen on provided port, on all network interfaces.

--- express-server/server.js
+++ express-server/server.js
@@ -22,3 +22,6 @@ app.set('port', port);
 
 // Create HTTP server.
 const server = http.createServer(app);
+
+// Listen on provided port, on all network interfaces.
+server.listen(port, () => console.log(`API running on localhost:${port}`));
