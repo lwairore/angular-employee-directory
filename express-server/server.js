ab578a322b9e5febb4d48c39ee46bb65b49b0b80
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 20:56:18 +0300

    - Create HTTP server.

--- express-server/server.js
+++ express-server/server.js
@@ -19,3 +19,6 @@ app.use('/', api);
 // Get port from environment and store in Express.
 const port = process.env.PORT || '3000';
 app.set('port', port);
+
+// Create HTTP server.
+const server = http.createServer(app);
