29f12310b0ee8df489fb872459ac0c994422c341
Lucas Wairore Karangu <kwairore@gmail.com>
2022-02-02 21:12:41 +0300

    - Add 'MongoDB' URL from the 'docker-compose' file.

--- express-server/routes/api.js
+++ express-server/routes/api.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 
+// MongoDB URL from the docker-compose file
+const dbHost = 'mongodb://database/mean-docker';
+
 // GET api listing.
 router.get('/', (req, res) => {
     res.send('api works');
